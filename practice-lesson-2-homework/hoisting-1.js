a89c3ccd2b537ef987fbefd21e20de628fdac31e
Your Name <you@example.com>
2017-04-19 10:19:05 +0400

    first commit

---
+++ practice-lesson-2-homework/hoisting-1.js
@@ -0,0 +1,60 @@
+// hoisting 1
+// https://github.com/davebalmer/turtlewax/blob/master/hsv.js
+// we know that only variables defined with var are hoisted.
+// function arguments don't need to be explicitly hoisted
+
+hsvtorgb = function(h, s, v) {
+	//hoisted variables and function
+	var i;
+	var f;
+	var p;
+	var q;
+	var t;
+	
+	function format(r, g, b) {
+		return "rgb(" + Math.round(r * 255) + "," + Math.round(g * 255) + "," + Math.round(b * 255) + ")";
+	}
+
+	// after hoisting part
+	
+	if (s == 0) {
+		return format(v, v, v);
+	}
+
+	h = ((h + 360) % 360) / 60;
+
+	i = Math.floor(h);
+	f = h - i;
+	p = v * (1 - s);
+	q = v * (1 - s * f);
+	t = v * (1 - s * (1 - f));
+
+	
+
+	switch(i) {
+	case 0:
+		r = v; g = t; b = p;
+		break;
+
+	case 1:
+		r = q; g = v; b = p;
+		break;
+
+	case 2:
+		r = p; g = v; b = t;
+		break;
+
+	case 3:
+		r = p; g = q; b = v;
+		break;
+
+	case 4:
+		r = t; g = p; b = v;
+		break;
+
+	default:
+		r = v; g = p; b = q;
+	}
+
+	return format(r, g, b);
+};
